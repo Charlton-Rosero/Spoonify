1e5e76e76d85909cde3a75097ba60064bc2695bf
Charlton-Rosero <charltonprosero@gmail.com>
2023-10-01 19:59:28 -0400

    Why do i keep writing these comments? im working on this alone. Anyways, created adapter for recyclerview.

---
+++ app/src/main/java/com/example/spoonify/adapters/RecipesAdapters.kt
@@ -0,0 +1,55 @@
+package com.example.spoonify.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.spoonify.databinding.RecipesRowLayoutBinding
+import com.example.spoonify.models.FoodRecipe
+import com.example.spoonify.models.Result
+import com.example.spoonify.util.RecipesDiffUtil
+
+class RecipesAdapters: RecyclerView.Adapter<RecipesAdapters.MyViewHolder>() {
+
+    private var recipe = emptyList<Result>()
+
+    class MyViewHolder(
+        private val binding: RecipesRowLayoutBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(result: Result){
+            binding.result = result
+            binding.executePendingBindings()
+        }
+
+        companion object{
+            fun from(parent: ViewGroup): MyViewHolder{
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RecipesRowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+       val currentResult = recipe[position]
+        holder.bind(currentResult)
+
+    }
+
+    override fun getItemCount(): Int {
+        return recipe.size
+    }
+
+    fun setData(newData: FoodRecipe){
+        val recipesDiffUtil = RecipesDiffUtil(recipe, newData.results)
+        val diffUtilResult = DiffUtil.calculateDiff(recipesDiffUtil)
+        recipe = newData.results
+        diffUtilResult.dispatchUpdatesTo(this)
+
+    }
+}
