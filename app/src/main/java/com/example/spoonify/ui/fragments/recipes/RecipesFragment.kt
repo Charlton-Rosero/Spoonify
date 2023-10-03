929fc5b3a18fbff82932f9ee8587ac65798be51d
Charlton-Rosero <charltonprosero@gmail.com>
2023-10-02 21:39:51 -0400

    Created recipes fragment viewmodel

--- app/src/main/java/com/example/spoonify/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/spoonify/ui/fragments/recipes/RecipesFragment.kt
@@ -7,10 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
-import com.example.spoonify.MainViewModel
+import com.example.spoonify.viewmodel.MainViewModel
 import com.example.spoonify.R
 import com.example.spoonify.adapters.RecipesAdapter
-import com.example.spoonify.databinding.FragmentRecipesBinding
 import com.example.spoonify.util.NetworkResult
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -32,54 +31,42 @@ class RecipesFragment : Fragment() {
 //        setupRecyclerView()
 
     }
-//    private fun requestApiData() {
-//        mainViewModel.getRecipes(applyQueries())
-//        mainViewModel.recipesResponse.observe(viewLifecycleOwner,{ response ->
-//            when(response){
-//                is NetworkResult.Success -> {
-//                    hideShimmerEffect()
-//                    response.data?.let { mAdapter.setData(it) }
-//                }
-//                is NetworkResult.Error -> {
-//                    hideShimmerEffect()
-//                    Toast.makeText(
-//                        requireContext(),
-//                        response.message.toString(),
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                }
-//                is NetworkResult.Loading ->{
-//                    showShimmerEffect()
-//                }
-//            }
-//        })
-//    }
-//
-//    private fun applyQueries(): Map<String, String> {
-//        val queries: HashMap<String, String> = HashMap()
-//
-//        queries["number"]="50"
-//        queries["apiKey"]="b4d820841ab045029449379781b3d1a5"
-//        queries["type"]="snack"
-//        queries["diet"]="vegan"
-//        queries["addRecipeInformation"]="true"
-//        queries["fillIngredients"]="true"
-//
-//        return queries
-//    }
-//
-//    private fun setupRecyclerView() {
-//        recipesView.recyclerview.adapter = mAdapter
-//        recipesView.recyclerview.layoutManager = LinearLayoutManager(requireContext())
-//        showShimmerEffect()
-//    }
-//
-//    private fun showShimmerEffect() {
-//        recipesView.recyclerview.showShimmer()
-//    }
-//
-//    private fun hideShimmerEffect() {
-//        recipesView.recyclerview.hideShimmer()
-//    }
+    private fun requestApiData() {
+        mainViewModel.getRecipes(applyQueries())
+        mainViewModel.recipesResponse.observe(viewLifecycleOwner,{ response ->
+            when(response){
+                is NetworkResult.Success -> {
+                    hideShimmerEffect()
+                    response.data?.let { mAdapter.setData(it) }
+                }
+                is NetworkResult.Error -> {
+                    hideShimmerEffect()
+                    Toast.makeText(
+                        requireContext(),
+                        response.message.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                is NetworkResult.Loading ->{
+                    showShimmerEffect()
+                }
+            }
+        })
+    }
+
+
+    private fun setupRecyclerView() {
+        recipesView.recyclerview.adapter = mAdapter
+        recipesView.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
+    }
+
+    private fun showShimmerEffect() {
+        recipesView.recyclerview.showShimmer()
+    }
+
+    private fun hideShimmerEffect() {
+        recipesView.recyclerview.hideShimmer()
+    }
 
 }
--- app/src/main/java/com/example/spoonify/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/spoonify/viewmodel/MainViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.spoonify
+package com.example.spoonify.viewmodel
 
 import android.app.Application
 import android.content.Context
