745d243c83a7ca830b948fba7aa2f36a7d674c13
Charlton-Rosero <charltonprosero@gmail.com>
2023-10-03 20:17:24 -0400

    Entity created.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,8 +93,8 @@ dependencies {
 
 
     // Coroutines
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     // Lifecycle
     implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
