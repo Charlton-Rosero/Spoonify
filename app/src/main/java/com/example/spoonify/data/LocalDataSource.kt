fc45d2f60f2c7d1a3edabf1dc342119c6806560d
Charlton-Rosero <charltonprosero@gmail.com>
2023-10-03 21:08:24 -0400

    offline caching implemented

---
+++ app/src/main/java/com/example/spoonify/data/LocalDataSource.kt
@@ -0,0 +1,22 @@
+package com.example.spoonify.data
+
+import com.example.spoonify.data.database.RecipesDao
+import com.example.spoonify.data.database.RecipesEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class LocalDataSource @Inject constructor(
+    private val recipesDao: RecipesDao
+
+) {
+
+    fun readDatabase(): Flow<List<RecipesEntity>>{
+        return recipesDao.readRecipes()
+    }
+
+    suspend fun insertRecipes(recipesEntity: RecipesEntity){
+        recipesDao.insertRecipes(recipesEntity)
+    }
+
+
+}
