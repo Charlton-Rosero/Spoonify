f2b52a2d8eb02a2f8283b83c41ca32799f26b1c0
Charlton-Rosero <charltonprosero@gmail.com>
2023-10-10 20:45:44 -0400

    ughh.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,9 @@
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    kotlin("kapt") version "1.9.10"
     id ("com.google.secrets_gradle_plugin") version "0.4" apply false
-    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
 
 }
 buildscript {
