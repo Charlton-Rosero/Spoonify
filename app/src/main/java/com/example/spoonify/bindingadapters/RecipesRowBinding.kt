c4ac12560973e1af2fddadc7f5b043db443b3d92
Charlton-Rosero <charltonprosero@gmail.com>
2023-10-11 20:13:14 -0400

    created an extension function so readDatabase is observed only once

--- app/src/main/java/com/example/spoonify/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/example/spoonify/bindingadapters/RecipesRowBinding.kt
@@ -23,6 +23,7 @@ class RecipesRowBinding {
         fun loadImage(imageView: ImageView, imageUrl: String){
             imageView.load(imageUrl){
                 crossfade(600)
+                error(R.drawable.ic_error_placeholder)
             }
         }
         @JvmStatic
